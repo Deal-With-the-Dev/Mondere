b97ad1f79e9fc8bfd19c148ab3903438420df88d
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2020-08-02 20:09:39 -0400

    adding card prefabs

---
+++ Mondere/Assets/Scripts/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Card : MonoBehaviour
+    {
+        [SerializeField]
+        private Image TopLeft;
+
+        [SerializeField]
+        private Text title;
+        [SerializeField]
+        private Text description;
+    }
+}
